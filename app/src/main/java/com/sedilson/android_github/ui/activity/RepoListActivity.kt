0f79a2cdd699db8241447621c1152aa6f9f4553c
SEdilson <edilson.silva00@hotmail.com>
2020-03-03 22:34:58 -0300

    Adding layout of project

---
+++ app/src/main/java/com/sedilson/android_github/ui/activity/RepoListActivity.kt
@@ -0,0 +1,13 @@
+package com.sedilson.android_github.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sedilson.android_github.R
+
+class RepoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repo_list)
+    }
+}
